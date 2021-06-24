26c2355b7ee517470dca3df8c23da4fb34ca6650
wakrimhajar <hajar1wakrim@gmail.com>
2021-06-24 18:01:47 +0100

    edit

--- backend-gesimmo/resources/views/emails/TestMail.blade.php
+++ backend-gesimmo/resources/views/emails/TestMail.blade.php
@@ -6,7 +6,7 @@
 <body>
     <p>Bonjour {{ $details['prenom']  }} {{ $details['nom'] }};</p>
     <p>Nous vous remercions d'avoir fait confiance à notre société et nous vous souhaitons un bon séjour 
-            dans votre nouvelle location. Pour suivre l'état de vos paiements, un compte d'accès à notre plateforme. https:lien
+            dans votre nouvelle location. Pour suivre l'état de vos paiements, un compte d'accès à notre plateforme. http://gesimmoapp.000webhostapp.com/
     Vous a été crée.
     <br>
     Login : <u><b>{{ $details['title'] }} </b></u></p> 
